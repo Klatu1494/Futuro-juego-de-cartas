0ae8ad14598890dfb69bb1d32b4492ea2605a062
Luca <klatu1494@gmail.com>
2017-05-22 17:52:15 -0300

    added a class file template and moved the Square class into the appropriate folder

---
+++ classes/<class>.js
@@ -0,0 +1,19 @@
+/**
+ * @truefileoverview This is a class file template. Do not put into master.
+ *     Do not include into index.html.
+ * @fileoverview Contains the <class> class declaration and
+ *     can contain definitions of the class' prototype's properties.
+ */
+
+/**
+ * A class.
+ * @class
+ */
+class < class > {
+  /**
+   * Creates an instance of this class.
+   */
+  constructor() {
+
+  }
+}
