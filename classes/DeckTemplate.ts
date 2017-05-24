185f14f0c28aac106044c0317f42744e58688a4d
Edgar Carballo <karvayoEdgar@gmail.com>
2017-05-23 19:16:39 -0500

    DeckTemplate as .ts

---
+++ classes/DeckTemplate.ts
@@ -0,0 +1,40 @@
+/**
+ * @fileoverview Contains the DeckTemplate class declaration and
+ *     can contain definitions of the class' prototype's properties.
+ */
+
+/**
+ * @class
+ * @summary A template used to create the decks for each match.
+ */
+
+/**
+ * Represents a card instance
+ */
+class Card {
+  // Not implemented
+}
+
+class DeckTemplate {
+  private readonly _cards: Array<Card>
+  /**
+   * Creates a new deck template.
+   * @param {Array<Card>} Cards cards for the template.
+   */
+  constructor(cards: Card[] = []) {
+    this._cards = new Array<Card>(cards);
+  }
+
+  /**
+   * Gets the cards of the deck
+   */
+  get cards(): ReadonlyArray<Card> { return this._cards; }
+
+  /**
+   * Determines whether the deck template is empty.
+   * @return {boolean} Whether the deck template is empty.
+   */
+  isEmpty() {
+    return !this._cards.length;
+  }
+}
